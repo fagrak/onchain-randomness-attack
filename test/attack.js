522a0deba873d897f8cce07d001d7c4fb19e6caa
Furkan <63971790+fagrak@users.noreply.github.com>
2023-05-11 23:44:40 +0300

    first commit

---
+++ test/attack.js
@@ -0,0 +1,27 @@
+const { ethers, waffle } = require("hardhat");
+const { expect } = require("chai");
+const { BigNumber, utils } = require("ethers");
+
+describe("Attack", function () {
+    it("should be able to guess the exact number", async function () {
+
+        const Game = await ethers.getContractFactory("Game");
+        const game = await Game.deploy({ value: utils.parseEther("0.1") });
+        await game.deployed();
+
+        console.log("Game contract adderss", game.address);
+
+        const Attack = await ethers.getContractFactory("Attack");
+        const attack = await Attack.deploy(game.address);
+
+        console.log("Attack contract address", attack.address);
+
+        const tx = await attack.attack();
+        await tx.wait();
+
+        const balanceGame = await game.getBalance();
+
+        expect(balanceGame).to.equal(BigNumber.from(0));
+
+    });
+});
